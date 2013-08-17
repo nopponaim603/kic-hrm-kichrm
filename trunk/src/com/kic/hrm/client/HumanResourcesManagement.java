be95fa5f4f1b8a0645d5f3bef81243daae1816cf
noppon.w@vr.camt.info <noppon.w@vr.camt.info@b031e134-1f6c-318a-06de-4c14a77bf882>
2013-08-17 15:32:10 +0000

    Update Interface main,report and calendar

--- trunk/src/com/kic/hrm/client/HumanResourcesManagement.java
+++ trunk/src/com/kic/hrm/client/HumanResourcesManagement.java
@@ -27,6 +27,8 @@ import com.google.gwt.user.datepicker.client.DateBox;
 import com.google.gwt.user.client.ui.RadioButton;
 import com.google.gwt.user.datepicker.client.DatePicker;
 import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.PushButton;
+import com.google.gwt.user.cellview.client.CellTable;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -64,19 +66,17 @@ public class HumanResourcesManagement implements EntryPoint {
 		rootPanel.add(tabPanel, 10, 10);
 		tabPanel.setSize("780px", "580px");
 
-		FlowPanel flowPanel = new FlowPanel();
-		tabPanel.add(flowPanel, "New tab", false);
-		flowPanel.setSize("780px", "550px");
+		VerticalPanel verticalPanel_3 = new VerticalPanel();
+		tabPanel.add(verticalPanel_3, "หน้าแรก", false);
+		verticalPanel_3.setSize("780px", "550px");
 
-		Button btnNewButton = new Button("New button");
-		flowPanel.add(btnNewButton);
-
-		Button btnNewButton_1 = new Button("New button");
-		flowPanel.add(btnNewButton_1);
+		CellTable<Object> cellTable = new CellTable<Object>();
+		verticalPanel_3.add(cellTable);
+		cellTable.setWidth("521px");
 
 		VerticalPanel verticalPanel = new VerticalPanel();
 		verticalPanel.setVisible(false);
-		tabPanel.add(verticalPanel, "New tab", false);
+		tabPanel.add(verticalPanel, "แบบฟอร์มลางาน", false);
 		verticalPanel.setSize("780px", "550px");
 
 		HorizontalPanel horizontalPanel = new HorizontalPanel();
@@ -93,35 +93,39 @@ public class HumanResourcesManagement implements EntryPoint {
 		label.setDirectionEstimator(true);
 		grid_3.setWidget(0, 0, label);
 		label.setSize("151px", "24px");
-		
+
 		Grid grid_1 = new Grid(3, 2);
 		grid_1.setStyleName("gwt-panal");
 		grid_3.setWidget(1, 0, grid_1);
-		
-				Label label_1 = new Label("\u0E0A\u0E37\u0E48\u0E2D-\u0E19\u0E32\u0E21\u0E2A\u0E01\u0E38\u0E25");
-				label_1.setStyleName("gwt-panal");
-				grid_1.setWidget(0, 0, label_1);
-				label_1.setWidth("128px");
-				
-						Label label_2 = new Label("\u0E19\u0E32\u0E22 \u0E23\u0E27\u0E22\u0E17\u0E23\u0E31\u0E1E\u0E22\u0E4C \u0E2A\u0E27\u0E07\u0E2A\u0E27\u0E23\u0E23\u0E04\u0E4C");
-						label_2.setStyleName("gwt-panal");
-						grid_1.setWidget(0, 1, label_2);
-						
-								Label label_3 = new Label("\u0E23\u0E2B\u0E31\u0E2A\u0E1E\u0E19\u0E31\u0E01\u0E07\u0E32\u0E19");
-								label_3.setStyleName("gwt-panal");
-								grid_1.setWidget(1, 0, label_3);
-										
-												Label label_4 = new Label("5032165");
-												label_4.setStyleName("gwt-panal");
-												grid_1.setWidget(1, 1, label_4);
-								
-										Label label_5 = new Label("\u0E15\u0E33\u0E41\u0E2B\u0E19\u0E48\u0E07");
-										label_5.setStyleName("gwt-panal");
-										grid_1.setWidget(2, 0, label_5);
-										
-												Label label_6 = new Label("\u0E1E\u0E19\u0E31\u0E01\u0E07\u0E32\u0E19\u0E1B\u0E23\u0E30\u0E08\u0E33 (\u0E2A\u0E48\u0E27\u0E19\u0E07\u0E32\u0E19)");
-												label_6.setStyleName("gwt-panal");
-												grid_1.setWidget(2, 1, label_6);
+
+		Label label_1 = new Label(
+				"\u0E0A\u0E37\u0E48\u0E2D-\u0E19\u0E32\u0E21\u0E2A\u0E01\u0E38\u0E25");
+		label_1.setStyleName("gwt-panal");
+		grid_1.setWidget(0, 0, label_1);
+		label_1.setWidth("128px");
+
+		Label label_2 = new Label(
+				"\u0E19\u0E32\u0E22 \u0E23\u0E27\u0E22\u0E17\u0E23\u0E31\u0E1E\u0E22\u0E4C \u0E2A\u0E27\u0E07\u0E2A\u0E27\u0E23\u0E23\u0E04\u0E4C");
+		label_2.setStyleName("gwt-panal");
+		grid_1.setWidget(0, 1, label_2);
+
+		Label label_3 = new Label(
+				"\u0E23\u0E2B\u0E31\u0E2A\u0E1E\u0E19\u0E31\u0E01\u0E07\u0E32\u0E19");
+		label_3.setStyleName("gwt-panal");
+		grid_1.setWidget(1, 0, label_3);
+
+		Label label_4 = new Label("5032165");
+		label_4.setStyleName("gwt-panal");
+		grid_1.setWidget(1, 1, label_4);
+
+		Label label_5 = new Label("\u0E15\u0E33\u0E41\u0E2B\u0E19\u0E48\u0E07");
+		label_5.setStyleName("gwt-panal");
+		grid_1.setWidget(2, 0, label_5);
+
+		Label label_6 = new Label(
+				"\u0E1E\u0E19\u0E31\u0E01\u0E07\u0E32\u0E19\u0E1B\u0E23\u0E30\u0E08\u0E33 (\u0E2A\u0E48\u0E27\u0E19\u0E07\u0E32\u0E19)");
+		label_6.setStyleName("gwt-panal");
+		grid_1.setWidget(2, 1, label_6);
 
 		Grid grid_2 = new Grid(2, 1);
 		horizontalPanel.add(grid_2);
@@ -133,32 +137,33 @@ public class HumanResourcesManagement implements EntryPoint {
 		lblNewLabel.setStyleName("gwt-panal");
 		lblNewLabel.setDirectionEstimator(true);
 		lblNewLabel.setSize("205px", "24px");
-		
+
 		Grid grid = new Grid(3, 2);
 		grid.setStyleName("gwt-panal");
 		grid_2.setWidget(1, 0, grid);
-		
+
 		Label label_9 = new Label("\u0E25\u0E32\u0E01\u0E34\u0E08");
 		label_9.setStyleName("gwt-panal");
 		grid.setWidget(0, 0, label_9);
 		label_9.setWidth("128px");
-		
+
 		Label label_10 = new Label("10");
 		label_10.setStyleName("gwt-panal");
 		grid.setWidget(0, 1, label_10);
-		
+
 		Label label_11 = new Label("\u0E25\u0E32\u0E1B\u0E48\u0E27\u0E22");
 		label_11.setStyleName("gwt-panal");
 		grid.setWidget(1, 0, label_11);
-		
+
 		Label label_12 = new Label("10");
 		label_12.setStyleName("gwt-panal");
 		grid.setWidget(1, 1, label_12);
-		
-		Label label_13 = new Label("\u0E25\u0E32\u0E1E\u0E31\u0E01\u0E1C\u0E48\u0E2D\u0E19\u0E1B\u0E23\u0E30\u0E08\u0E33\u0E1B\u0E35");
+
+		Label label_13 = new Label(
+				"\u0E25\u0E32\u0E1E\u0E31\u0E01\u0E1C\u0E48\u0E2D\u0E19\u0E1B\u0E23\u0E30\u0E08\u0E33\u0E1B\u0E35");
 		label_13.setStyleName("gwt-panal");
 		grid.setWidget(2, 0, label_13);
-		
+
 		Label label_14 = new Label("10");
 		label_14.setStyleName("gwt-panal");
 		grid.setWidget(2, 1, label_14);
@@ -167,7 +172,8 @@ public class HumanResourcesManagement implements EntryPoint {
 		verticalPanel.add(verticalPanel_1);
 		verticalPanel_1.setSize("504px", "62px");
 
-		Label lblNewLabel_7 = new Label("\u0E21\u0E35\u0E04\u0E27\u0E32\u0E21\u0E1B\u0E23\u0E30\u0E2A\u0E07\u0E04\u0E4C\u0E17\u0E35\u0E48\u0E08\u0E30\u0E25\u0E32");
+		Label lblNewLabel_7 = new Label(
+				"\u0E21\u0E35\u0E04\u0E27\u0E32\u0E21\u0E1B\u0E23\u0E30\u0E2A\u0E07\u0E04\u0E4C\u0E17\u0E35\u0E48\u0E08\u0E30\u0E25\u0E32");
 		verticalPanel_1.add(lblNewLabel_7);
 		lblNewLabel_7.setWidth("472px");
 
@@ -194,7 +200,8 @@ public class HumanResourcesManagement implements EntryPoint {
 		verticalPanel.add(verticalPanel_2);
 		verticalPanel_2.setSize("504px", "62px");
 
-		Label label_7 = new Label("\u0E2A\u0E32\u0E40\u0E2B\u0E15\u0E38\u0E02\u0E2D\u0E07\u0E01\u0E32\u0E23\u0E25\u0E32");
+		Label label_7 = new Label(
+				"\u0E2A\u0E32\u0E40\u0E2B\u0E15\u0E38\u0E02\u0E2D\u0E07\u0E01\u0E32\u0E23\u0E25\u0E32");
 		verticalPanel_2.add(label_7);
 		label_7.setWidth("472px");
 
@@ -207,7 +214,8 @@ public class HumanResourcesManagement implements EntryPoint {
 		verticalPanel.add(horizontalPanel_3);
 		horizontalPanel_3.setSize("212px", "33px");
 
-		Label label_8 = new Label("\u0E25\u0E32\u0E15\u0E31\u0E49\u0E07\u0E41\u0E15\u0E48\u0E27\u0E31\u0E19\u0E17\u0E35\u0E48");
+		Label label_8 = new Label(
+				"\u0E25\u0E32\u0E15\u0E31\u0E49\u0E07\u0E41\u0E15\u0E48\u0E27\u0E31\u0E19\u0E17\u0E35\u0E48");
 		horizontalPanel_3.add(label_8);
 		label_8.setSize("107px", "27px");
 
@@ -215,9 +223,10 @@ public class HumanResourcesManagement implements EntryPoint {
 		horizontalPanel_3.add(dateBox);
 		dateBox.setWidth("189px");
 
-		Label lblNewLabel_8 = new Label("\u0E08\u0E33\u0E19\u0E27\u0E19\u0E27\u0E31\u0E19\u0E17\u0E35\u0E48\u0E15\u0E49\u0E2D\u0E07\u0E01\u0E32\u0E23\u0E25\u0E32");
+		Label lblNewLabel_8 = new Label(
+				"\u0E08\u0E33\u0E19\u0E27\u0E19\u0E27\u0E31\u0E19\u0E17\u0E35\u0E48\u0E15\u0E49\u0E2D\u0E07\u0E01\u0E32\u0E23\u0E25\u0E32");
 		horizontalPanel_3.add(lblNewLabel_8);
-		lblNewLabel_8.setWidth("102px");
+		lblNewLabel_8.setWidth("141px");
 
 		ListBox comboBox = new ListBox();
 		comboBox.addItem("1");
@@ -232,7 +241,8 @@ public class HumanResourcesManagement implements EntryPoint {
 		comboBox.addItem("10");
 		horizontalPanel_3.add(comboBox);
 
-		Label lblNewLabel_9 = new Label("\u0E08\u0E19\u0E16\u0E36\u0E07\u0E27\u0E31\u0E19\u0E17\u0E35\u0E48 11  \u0E01.\u0E04.  2556");
+		Label lblNewLabel_9 = new Label(
+				"\u0E08\u0E19\u0E16\u0E36\u0E07\u0E27\u0E31\u0E19\u0E17\u0E35\u0E48 11  \u0E01.\u0E04.  2556");
 		horizontalPanel_3.add(lblNewLabel_9);
 		lblNewLabel_9.setWidth("169px");
 
@@ -240,7 +250,8 @@ public class HumanResourcesManagement implements EntryPoint {
 		verticalPanel.add(horizontalPanel_4);
 		horizontalPanel_4.setSize("501px", "45px");
 
-		Label lblNewLabel_10 = new Label("\u0E40\u0E1A\u0E2D\u0E23\u0E4C\u0E15\u0E34\u0E14\u0E15\u0E48\u0E2D\u0E40\u0E23\u0E48\u0E07\u0E14\u0E48\u0E27\u0E19");
+		Label lblNewLabel_10 = new Label(
+				"\u0E40\u0E1A\u0E2D\u0E23\u0E4C\u0E15\u0E34\u0E14\u0E15\u0E48\u0E2D\u0E40\u0E23\u0E48\u0E07\u0E14\u0E48\u0E27\u0E19");
 		horizontalPanel_4.add(lblNewLabel_10);
 
 		ListBox listBox = new ListBox();
@@ -262,45 +273,47 @@ public class HumanResourcesManagement implements EntryPoint {
 		lblNewLabel_11.setWidth("165px");
 
 		ListBox comboBox_1 = new ListBox();
-		comboBox_1.addItem("\u0E19\u0E32\u0E22 \u0E2A\u0E21\u0E08\u0E34\u0E15\u0E23 \u0E21\u0E32\u0E01\u0E31\u0E19\u0E41\u0E25\u0E49\u0E27");
+		comboBox_1
+				.addItem("\u0E19\u0E32\u0E22 \u0E2A\u0E21\u0E08\u0E34\u0E15\u0E23 \u0E21\u0E32\u0E01\u0E31\u0E19\u0E41\u0E25\u0E49\u0E27");
 		horizontalPanel_5.add(comboBox_1);
 		comboBox_1.setWidth("186px");
-		
+
 		HorizontalPanel horizontalPanel_6 = new HorizontalPanel();
-		horizontalPanel_6.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
+		horizontalPanel_6
+				.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
 		verticalPanel.add(horizontalPanel_6);
 		horizontalPanel_6.setWidth("780px");
-		
+
 		Button btnNewButton_5 = new Button("\u0E15\u0E01\u0E25\u0E07");
 		horizontalPanel_6.add(btnNewButton_5);
 		btnNewButton_5.setWidth("70px");
-		
+
 		Button btnNewButton_6 = new Button("New button");
 		btnNewButton_6.setText("\u0E22\u0E01\u0E40\u0E25\u0E34\u0E01");
 		horizontalPanel_6.add(btnNewButton_6);
 		btnNewButton_6.setWidth("70px");
 
-		FlowPanel flowPanel_2 = new FlowPanel();
-		tabPanel.add(flowPanel_2, "New tab", false);
-		flowPanel_2.setSize("780px", "550px");
-
-		HorizontalPanel horizontalPanel_2 = new HorizontalPanel();
-		horizontalPanel_2.setSpacing(1);
-		horizontalPanel_2.setBorderWidth(1);
-
-		flowPanel_2.add(horizontalPanel_2);
-		horizontalPanel_2.setSize("174px", "28px");
-
-		Button btnNewButton_2 = new Button("New button");
-		horizontalPanel_2.setCellWidth(btnNewButton_2, null);
-		horizontalPanel_2.add(btnNewButton_2);
-
-		Button btnNewButton_3 = new Button("New button");
-		horizontalPanel_2.add(btnNewButton_3);
+		VerticalPanel verticalPanel_4 = new VerticalPanel();
+		tabPanel.add(verticalPanel_4, "รายงานการลา", false);
+		verticalPanel_4.setSize("780px", "550px");
+		
+		Label lblNewLabel_1 = new Label("New label");
+		verticalPanel_4.add(lblNewLabel_1);
+		
+		ListBox comboBox_2 = new ListBox();
+		verticalPanel_4.add(comboBox_2);
+		
+		CellTable<Object> cellTable_1 = new CellTable<Object>();
+		verticalPanel_4.add(cellTable_1);
+		cellTable_1.setSize("558px", "242px");
 
 		FlowPanel flowPanel_3 = new FlowPanel();
-		tabPanel.add(flowPanel_3, "New tab", false);
+		tabPanel.add(flowPanel_3, "ปฏิทินการลา", false);
 		flowPanel_3.setSize("780px", "580px");
+
+		DatePicker datePicker = new DatePicker();
+		flowPanel_3.add(datePicker);
+		datePicker.setSize("625px", "325px");
 
 		// Create the popup dialog box
 		final DialogBox dialogBox = new DialogBox();
