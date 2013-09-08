9b092c41728969e30d14a4dfc6e2b4e23b3a0f49
noppon.w@vr.camt.info <noppon.w@vr.camt.info@b031e134-1f6c-318a-06de-4c14a77bf882>
2013-09-08 06:41:12 +0000

    Test send e-mail success

--- trunk/src/com/kic/hrm/client/event/ApplyLeavingEvent.java
+++ trunk/src/com/kic/hrm/client/event/ApplyLeavingEvent.java
@@ -6,13 +6,11 @@ public class ApplyLeavingEvent extends GwtEvent<ApplyLeavingEventHandler>{
 	public static Type<ApplyLeavingEventHandler> TYPE = new Type<ApplyLeavingEventHandler>();
 	@Override
 	public Type<ApplyLeavingEventHandler> getAssociatedType() {
-		// TODO Auto-generated method stub
 		return TYPE;
 	}
 
 	@Override
 	protected void dispatch(ApplyLeavingEventHandler handler) {
-		// TODO Auto-generated method stub
 		handler.onApplyLeaving(this);
 	}
 
--- trunk/src/com/kic/hrm/client/event/ApplyLeavingEventHandler.java
+++ trunk/src/com/kic/hrm/client/event/ApplyLeavingEventHandler.java
@@ -2,7 +2,6 @@ package com.kic.hrm.client.event;
 
 import com.google.gwt.event.shared.EventHandler;
 
-
 public interface ApplyLeavingEventHandler extends EventHandler{
 	void onApplyLeaving(ApplyLeavingEvent event);
 }
