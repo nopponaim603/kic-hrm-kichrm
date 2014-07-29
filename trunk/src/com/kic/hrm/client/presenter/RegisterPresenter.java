279b0c6bea8be6bf143edf1e805da833cf22bf40
noppon.w@vr.camt.info <noppon.w@vr.camt.info@b031e134-1f6c-318a-06de-4c14a77bf882>
2014-07-29 09:58:28 +0000

    Last Day Update. 	Bug Path data model.

--- trunk/src/com/kic/hrm/client/presenter/RegisterPresenter.java
+++ trunk/src/com/kic/hrm/client/presenter/RegisterPresenter.java
@@ -1,15 +1,23 @@
 package com.kic.hrm.client.presenter;
 
+import javax.servlet.annotation.ServletSecurity.EmptyRoleSemantic;
+
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.shared.HandlerManager;
+import com.google.gwt.resources.css.ast.HasSelectors;
 import com.google.gwt.user.client.History;
+import com.google.gwt.user.client.ui.HasName;
+import com.google.gwt.user.client.ui.HasValue;
 import com.google.gwt.user.client.ui.HasWidgets;
 import com.google.gwt.user.client.ui.Widget;
 import com.kic.hrm.client.GreetingServiceAsync;
 import com.kic.hrm.client.event.ProfileUpdateEvent;
 import com.kic.hrm.client.presenter.HumanResourcesManagementPresenter.Display;
+
+import com.kic.hrm.data.model.Employee;
 
 public class RegisterPresenter implements Presenter{
 
@@ -18,18 +26,34 @@ public class RegisterPresenter implements Presenter{
 
 		HasClickHandlers getSubmit();
 		HasClickHandlers getCancel();
-
+		
+		HasValue<String> getWorkID();
+		HasName getSex();
+		HasValue<String> getName();
+		HasValue<String> getSurname();
+		HasValue<String> getNameT();
+		HasValue<String> getSurnameT();
+		HasValue<String> getShortname();
+		
+		HasName  getRole();
+		HasName  getSegment();
+		HasValue<String> getEmail();
+		HasValue<String> getPhone();
+		
 	}
 	
 	private final GreetingServiceAsync rpcService;
 	private final HandlerManager eventBus;
 	private final Display display;
-	
+	public Employee m_employee;
 	public RegisterPresenter(GreetingServiceAsync rpcService,HandlerManager eventBus, Display view) {
 		// TODO Auto-generated constructor stub
 		this.rpcService = rpcService;
 		this.eventBus = eventBus;
 		this.display = view;
+		m_employee = new Employee();
+		//display.getWorkID().getValue()
+		//display.getSex().getName()
 		bind();
 		
 	}
@@ -39,8 +63,7 @@ public class RegisterPresenter implements Presenter{
 		display.getSubmit().addClickHandler(new ClickHandler() {   
 		      public void onClick(ClickEvent event) {
 		    	  System.out.println("getSubmit  : on Click");
-		    	  eventBus.fireEvent(new ProfileUpdateEvent());
-		    	  History.fireCurrentHistoryState();
+		    	  //doSave();
 		      }
 		    });
 		
@@ -58,5 +81,28 @@ public class RegisterPresenter implements Presenter{
 		// TODO Auto-generated method stub
 			container.clear();
 			container.add(display.asWidget());
+	}
+	
+	private void doSave() {
+		
+		/*
+		 * 
+		m_employee.setM_employeeID(Integer.parseInt(display.getWorkID().getValue()));
+		m_employee.setM_sex(Employee.sex.valueOf(display.getSex().getName()));
+		m_employee.setM_name(display.getName().getValue());
+		m_employee.setM_surname(display.getSurname().getValue());
+		m_employee.setM_nameT(display.getNameT().getValue());
+		m_employee.setM_surnameT(display.getSurnameT().getValue());
+		m_employee.setM_shortName(display.getShortname().getValue());
+		m_employee.setM_role(Employee.role.valueOf(display.getRole().getName()));
+		m_employee.setM_segment(Employee.segment.valueOf(display.getSegment().getName()));
+		m_employee.setM_email(display.getEmail().getValue());
+		m_employee.setM_phone(display.getPhone().getValue());
+		
+		System.out.println("Test Value");
+		System.out.println(m_employee.getM_name() + " : " + m_employee.getM_employeeID() + " : " + m_employee.getM_sex());
+		
+		*/
+		//eventBus.fireEvent(new ProfileUpdateEvent());
 	}
 }
