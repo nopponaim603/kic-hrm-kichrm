77abdf38115e1d31338a0548042c97642b10972f
noppon.w@vr.camt.info <noppon.w@vr.camt.info@b031e134-1f6c-318a-06de-4c14a77bf882>
2014-08-07 17:27:45 +0000

    Midnight Update. 	Change Class Name Register -> Profile and Profile -> Leave.

--- src/com/kic/hrm/client/view/ProfileView.java
+++ src/com/kic/hrm/client/view/ProfileView.java
@@ -1,273 +1,265 @@
 package com.kic.hrm.client.view;
-
-import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.event.dom.client.HasChangeHandlers;
+import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.resources.css.ast.HasSelectors;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.FlexTable;
-import com.google.gwt.user.client.ui.HasHorizontalAlignment;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.ListBox;
-import com.google.gwt.user.client.ui.RadioButton;
-import com.google.gwt.user.client.ui.TabPanel;
-import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.HasName;
+import com.google.gwt.user.client.ui.HasValue;
 import com.google.gwt.user.client.ui.VerticalPanel;
-import com.google.gwt.user.datepicker.client.DateBox;
-import com.google.gwt.user.client.ui.DecoratedStackPanel;
-import com.google.gwt.user.client.ui.StackPanel;
-import com.google.gwt.user.client.ui.StackLayoutPanel;
-import com.google.gwt.dom.client.Style.Unit;
-import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Widget;
+import com.kic.hrm.client.presenter.ProfilePresenter;
+import com.google.gwt.user.client.ui.LongBox;
+import com.google.gwt.user.client.ui.IntegerBox;
 
-public class ProfileView extends Composite {
+public class ProfileView  extends Composite implements ProfilePresenter.Display{
+	
+	private final Button SubmitButton;
+	private final Button CancelButton;
+	private final TextBox WorkIDTextBox;
+	private final ListBox SexTextBox;
+	private final TextBox NameTextBox;
+	private final TextBox SurnameTextBox;
+	private final TextBox NameTTextBox;
+	private final TextBox SurnameTTextBox;
+	private final TextBox ShortNameTextBox;
+	private final ListBox RoleTextBox;
+	private final ListBox SegmentTextBox;
+	private final TextBox EmailTextBox;
+	private final TextBox PhoneTextBox;
 	
 	public ProfileView() {
-		TabPanel tabPanel = new TabPanel();
-		tabPanel.setAnimationEnabled(true);
-		initWidget(tabPanel);
-		tabPanel.setSize("780px", "580px");
+		// TODO Auto-generated constructor stub
+		VerticalPanel verticalPanel = new VerticalPanel();
+		initWidget(verticalPanel);
 		
-		VerticalPanel verticalPanelLeavingForm = new VerticalPanel();
-		verticalPanelLeavingForm.setVisible(false);
-		tabPanel.add(verticalPanelLeavingForm, "Leaving Form", false);
-		verticalPanelLeavingForm.setSize("780px", "550px");
+		Grid grid = new Grid(12, 2);
+		verticalPanel.add(grid);
 		
-		FlexTable flexTable = new FlexTable();
-		verticalPanelLeavingForm.add(flexTable);
-		flexTable.setSize("760px", "57px");
+		Label lblWorkid = new Label("WorkID");
+		grid.setWidget(0, 0, lblWorkid);
 		
-		Label label = new Label(
-				"\u0E27\u0E31\u0E19\u0E17\u0E35\u0E48 17 \u0E2A\u0E34\u0E07\u0E2B\u0E32\u0E04\u0E21 2556 ");
-		flexTable.setWidget(0, 0, label);
-		label.setStyleName("gwt-panal");
-		label.setDirectionEstimator(true);
-		label.setSize("151px", "24px");
-
-		Label lblNewLabel = new Label(
-				"\u0E08\u0E33\u0E19\u0E27\u0E19\u0E27\u0E31\u0E19\u0E25\u0E32\u0E2A\u0E30\u0E2A\u0E21");
-		flexTable.setWidget(0, 2, lblNewLabel);
-		lblNewLabel.setStyleName("gwt-panal");
-		lblNewLabel.setDirectionEstimator(true);
-		lblNewLabel.setSize("106px", "24px");
-
-		Label label_1 = new Label(
-				"\u0E0A\u0E37\u0E48\u0E2D-\u0E19\u0E32\u0E21\u0E2A\u0E01\u0E38\u0E25");
-		flexTable.setWidget(1, 0, label_1);
-		label_1.setStyleName("gwt-panal");
-		label_1.setWidth("128px");
-
-		Label label_2 = new Label(
-				"\u0E19\u0E32\u0E22 \u0E23\u0E27\u0E22\u0E17\u0E23\u0E31\u0E1E\u0E22\u0E4C \u0E2A\u0E27\u0E07\u0E2A\u0E27\u0E23\u0E23\u0E04\u0E4C");
-		flexTable.setWidget(1, 1, label_2);
-		label_2.setStyleName("gwt-panal");
-
-		Label label_9 = new Label("\u0E25\u0E32\u0E01\u0E34\u0E08");
-		flexTable.setWidget(1, 2, label_9);
-		label_9.setStyleName("gwt-panal");
-		label_9.setWidth("46px");
-
-		Label label_10 = new Label("10");
-		flexTable.setWidget(1, 3, label_10);
-		label_10.setStyleName("gwt-panal");
-
-		Label label_3 = new Label(
-				"\u0E23\u0E2B\u0E31\u0E2A\u0E1E\u0E19\u0E31\u0E01\u0E07\u0E32\u0E19");
-		flexTable.setWidget(2, 0, label_3);
-		label_3.setWidth("99px");
-		label_3.setStyleName("gwt-panal");
-
-		Label label_4 = new Label("5032165");
-		flexTable.setWidget(2, 1, label_4);
-		label_4.setStyleName("gwt-panal");
-
-		Label label_11 = new Label("\u0E25\u0E32\u0E1B\u0E48\u0E27\u0E22");
-		flexTable.setWidget(2, 2, label_11);
-		label_11.setWidth("55px");
-		label_11.setStyleName("gwt-panal");
-
-		Label label_12 = new Label("10");
-		flexTable.setWidget(2, 3, label_12);
-		label_12.setStyleName("gwt-panal");
-
-		Label label_5 = new Label("\u0E15\u0E33\u0E41\u0E2B\u0E19\u0E48\u0E07");
-		flexTable.setWidget(3, 0, label_5);
-		label_5.setWidth("115px");
-		label_5.setStyleName("gwt-panal");
-
-		Label label_6 = new Label(
-				"\u0E1E\u0E19\u0E31\u0E01\u0E07\u0E32\u0E19\u0E1B\u0E23\u0E30\u0E08\u0E33 (\u0E2A\u0E48\u0E27\u0E19\u0E07\u0E32\u0E19)");
-		flexTable.setWidget(3, 1, label_6);
-		label_6.setStyleName("gwt-panal");
-
-		Label label_13 = new Label(
-				"\u0E25\u0E32\u0E1E\u0E31\u0E01\u0E1C\u0E48\u0E2D\u0E19\u0E1B\u0E23\u0E30\u0E08\u0E33\u0E1B\u0E35");
-		flexTable.setWidget(3, 2, label_13);
-		label_13.setWidth("110px");
-		label_13.setStyleName("gwt-panal");
-
-		Label label_14 = new Label("10");
-		flexTable.setWidget(3, 3, label_14);
-		label_14.setStyleName("gwt-panal");
-		
-		VerticalPanel verticalPanel_1 = new VerticalPanel();
-		verticalPanelLeavingForm.add(verticalPanel_1);
-		verticalPanel_1.setSize("504px", "62px");
-		
-		Label lblNewLabel_7 = new Label(
-				"\u0E21\u0E35\u0E04\u0E27\u0E32\u0E21\u0E1B\u0E23\u0E30\u0E2A\u0E07\u0E04\u0E4C\u0E17\u0E35\u0E48\u0E08\u0E30\u0E25\u0E32");
-		verticalPanel_1.add(lblNewLabel_7);
-		lblNewLabel_7.setWidth("472px");
-
-		HorizontalPanel horizontalPanel_1 = new HorizontalPanel();
-		verticalPanel_1.add(horizontalPanel_1);
-		
-		RadioButton rdbtnNewRadioButton = new RadioButton("new name",
-				"\u0E25\u0E32\u0E01\u0E34\u0E08");
-		horizontalPanel_1.add(rdbtnNewRadioButton);
-
-		RadioButton rdbtnNewRadioButton_1 = new RadioButton("new name",
-				"\u0E25\u0E32\u0E1B\u0E48\u0E27\u0E22");
-		horizontalPanel_1.add(rdbtnNewRadioButton_1);
-
-		RadioButton rdbtnNewRadioButton_2 = new RadioButton("new name",
-				"\u0E25\u0E32\u0E1E\u0E31\u0E01\u0E1C\u0E48\u0E2D\u0E19");
-		horizontalPanel_1.add(rdbtnNewRadioButton_2);
-
-		RadioButton radioButton = new RadioButton("new name",
-				"\u0E2D\u0E37\u0E48\u0E19 \u0E46");
-		horizontalPanel_1.add(radioButton);
-		
-		VerticalPanel verticalPanel_2 = new VerticalPanel();
-		verticalPanelLeavingForm.add(verticalPanel_2);
-		verticalPanel_2.setSize("504px", "62px");
-		
-		Label label_7 = new Label(
-				"\u0E2A\u0E32\u0E40\u0E2B\u0E15\u0E38\u0E02\u0E2D\u0E07\u0E01\u0E32\u0E23\u0E25\u0E32");
-		verticalPanel_2.add(label_7);
-		label_7.setWidth("472px");
-
 		TextBox textBox = new TextBox();
-		textBox.setText("\u0E40\u0E19\u0E37\u0E48\u0E2D\u0E07\u0E21\u0E32\u0E08\u0E32\u0E01");
-		verticalPanel_2.add(textBox);
-		textBox.setSize("504px", "55px");
+		grid.setWidget(0, 1, textBox);
 		
-		HorizontalPanel horizontalPanel_3 = new HorizontalPanel();
-		verticalPanelLeavingForm.add(horizontalPanel_3);
-		horizontalPanel_3.setSize("212px", "33px");
+		Label lblSec = new Label("sex");
+		grid.setWidget(1, 0, lblSec);
 		
-		Label label_8 = new Label(
-				"\u0E25\u0E32\u0E15\u0E31\u0E49\u0E07\u0E41\u0E15\u0E48\u0E27\u0E31\u0E19\u0E17\u0E35\u0E48");
-		horizontalPanel_3.add(label_8);
-		label_8.setSize("107px", "27px");
-
-		DateBox dateBox = new DateBox();
-		horizontalPanel_3.add(dateBox);
-		dateBox.setWidth("189px");
-
-		Label lblNewLabel_8 = new Label(
-				"\u0E08\u0E33\u0E19\u0E27\u0E19\u0E27\u0E31\u0E19\u0E17\u0E35\u0E48\u0E15\u0E49\u0E2D\u0E07\u0E01\u0E32\u0E23\u0E25\u0E32");
-		horizontalPanel_3.add(lblNewLabel_8);
-		lblNewLabel_8.setWidth("141px");
-
 		ListBox comboBox = new ListBox();
-		comboBox.addItem("1");
-		comboBox.addItem("2");
-		comboBox.addItem("3");
-		comboBox.addItem("4");
-		comboBox.addItem("5");
-		comboBox.addItem("6");
-		comboBox.addItem("7");
-		comboBox.addItem("8");
-		comboBox.addItem("9");
-		comboBox.addItem("10");
-		horizontalPanel_3.add(comboBox);
-
-		Label lblNewLabel_9 = new Label(
-				"\u0E08\u0E19\u0E16\u0E36\u0E07\u0E27\u0E31\u0E19\u0E17\u0E35\u0E48 11  \u0E01.\u0E04.  2556");
-		horizontalPanel_3.add(lblNewLabel_9);
-		lblNewLabel_9.setWidth("169px");
+		comboBox.addItem("Mr");
+		comboBox.addItem("Mrs");
+		comboBox.addItem("Ms");
+		grid.setWidget(1, 1, comboBox);
 		
-		HorizontalPanel horizontalPanel_5 = new HorizontalPanel();
-		verticalPanelLeavingForm.add(horizontalPanel_5);
-		horizontalPanel_5.setSize("581px", "35px");
-
-		Label lblNewLabel_11 = new Label("New label");
-		horizontalPanel_5.add(lblNewLabel_11);
-		lblNewLabel_11.setWidth("165px");
-
+		Label lblName = new Label("Name");
+		grid.setWidget(2, 0, lblName);
+		
+		TextBox textBox_1 = new TextBox();
+		grid.setWidget(2, 1, textBox_1);
+		
+		Label lblSurname = new Label("Surname");
+		grid.setWidget(3, 0, lblSurname);
+		
+		TextBox textBox_2 = new TextBox();
+		grid.setWidget(3, 1, textBox_2);
+		
+		Label lblNamet = new Label("Name Thai");
+		grid.setWidget(4, 0, lblNamet);
+		
+		TextBox textBox_3 = new TextBox();
+		grid.setWidget(4, 1, textBox_3);
+		
+		Label lblSurnameThai = new Label("Surname Thai");
+		grid.setWidget(5, 0, lblSurnameThai);
+		
+		TextBox textBox_4 = new TextBox();
+		grid.setWidget(5, 1, textBox_4);
+		
+		Label lblShortname = new Label("Shortname");
+		grid.setWidget(6, 0, lblShortname);
+		
+		TextBox textBox_5 = new TextBox();
+		grid.setWidget(6, 1, textBox_5);
+		
+		Label lblRole = new Label("Role");
+		grid.setWidget(7, 0, lblRole);
+		
 		ListBox comboBox_1 = new ListBox();
-		comboBox_1
-				.addItem("\u0E19\u0E32\u0E22 \u0E2A\u0E21\u0E08\u0E34\u0E15\u0E23 \u0E21\u0E32\u0E01\u0E31\u0E19\u0E41\u0E25\u0E49\u0E27");
-		horizontalPanel_5.add(comboBox_1);
-		comboBox_1.setWidth("186px");
+		comboBox_1.addItem("Director");
+		comboBox_1.addItem("ProjectManager");
+		comboBox_1.addItem("Secretary");
+		comboBox_1.addItem("Administration");
+		comboBox_1.addItem("ChiefFinancialOfficer");
+		comboBox_1.addItem("ProcurementOfficer");
+		comboBox_1.addItem("ITOffice");
+		comboBox_1.addItem("ProjectOfficer");
+		comboBox_1.addItem("InformationOfficer");
+		comboBox_1.addItem("Advisor");
+		comboBox_1.addItem("Designer");
+		comboBox_1.addItem("ProjectCoordinator");
+		comboBox_1.addItem("ResearchAssistant");
+		comboBox_1.addItem("Researchers");
+		grid.setWidget(7, 1, comboBox_1);
 		
-		HorizontalPanel horizontalPanel_4 = new HorizontalPanel();
-		verticalPanelLeavingForm.add(horizontalPanel_4);
-		horizontalPanel_4.setSize("501px", "45px");
+		Label lblSegment = new Label("Segment");
+		grid.setWidget(8, 0, lblSegment);
 		
-		Label lblNewLabel_10 = new Label(
-				"\u0E40\u0E1A\u0E2D\u0E23\u0E4C\u0E15\u0E34\u0E14\u0E15\u0E48\u0E2D\u0E40\u0E23\u0E48\u0E07\u0E14\u0E48\u0E27\u0E19");
-		horizontalPanel_4.add(lblNewLabel_10);
-
-		ListBox listBox = new ListBox();
-		listBox.addItem("084-0417543");
-		listBox.addItem("086-6700339");
-		horizontalPanel_4.add(listBox);
-		listBox.setSize("212px", "42px");
-		listBox.setVisibleItemCount(5);
-
-		Button btnNewButton_4 = new Button("New button");
-		btnNewButton_4
-				.setText("\u0E40\u0E1E\u0E34\u0E48\u0E21\u0E40\u0E1A\u0E2D\u0E23\u0E4C\u0E42\u0E17\u0E23\u0E17\u0E31\u0E1E\u0E17\u0E4C\u0E15\u0E34\u0E14\u0E15\u0E48\u0E2D");
-		horizontalPanel_4.add(btnNewButton_4);
-
-		HorizontalPanel horizontalPanel_6 = new HorizontalPanel();
-		horizontalPanel_6
-				.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
-		verticalPanelLeavingForm.add(horizontalPanel_6);
-		horizontalPanel_6.setWidth("780px");
+		ListBox comboBox_2 = new ListBox();
+		comboBox_2.addItem("Director");
+		comboBox_2.addItem("Project");
+		comboBox_2.addItem("Office");
+		comboBox_2.addItem("Researchers");
+		comboBox_2.addItem("Advisor");
+		grid.setWidget(8, 1, comboBox_2);
 		
-		Button btnNewButton_5 = new Button("\u0E15\u0E01\u0E25\u0E07");
-		horizontalPanel_6.add(btnNewButton_5);
-		btnNewButton_5.setWidth("70px");
-
-		Button btnNewButton_6 = new Button("New button");
-		btnNewButton_6.setText("\u0E22\u0E01\u0E40\u0E25\u0E34\u0E01");
-		horizontalPanel_6.add(btnNewButton_6);
-		btnNewButton_6.setWidth("70px");
+		Label lblEmail = new Label("E-mail");
+		grid.setWidget(9, 0, lblEmail);
 		
-		DecoratedStackPanel decoratedStackPanel = new DecoratedStackPanel();
-		tabPanel.add(decoratedStackPanel, "New tab", true);
-		decoratedStackPanel.setSize("5cm", "3cm");
+		TextBox textBox_7 = new TextBox();
+		grid.setWidget(9, 1, textBox_7);
 		
-		Button btnNewButton = new Button("New button");
-		decoratedStackPanel.add(btnNewButton, "New widget", false);
-		btnNewButton.setSize("100%", "100%");
+		Label lblPhone = new Label("Phone");
+		grid.setWidget(10, 0, lblPhone);
 		
-		Button btnNewButton_1 = new Button("New button");
-		decoratedStackPanel.add(btnNewButton_1, "New widget", false);
-		btnNewButton_1.setSize("100%", "100%");
+		TextBox textBox_6 = new TextBox();
+		grid.setWidget(10, 1, textBox_6);
 		
-		StackPanel stackPanel = new StackPanel();
-		tabPanel.add(stackPanel, "New tab", false);
-		stackPanel.setSize("5cm", "3cm");
+		Button btnBack = new Button("Back");
+		grid.setWidget(11, 0, btnBack);
 		
-		Button btnNewButton_2 = new Button("New button");
-		stackPanel.add(btnNewButton_2, "New widget", false);
-		btnNewButton_2.setSize("100%", "100%");
+		Button btnNewButton = new Button("Submit");
+		grid.setWidget(11, 1, btnNewButton);
+		//verticalPanelLeavingForm.add(verticalPanel);
 		
-		Button btnNewButton_3 = new Button("New button");
-		stackPanel.add(btnNewButton_3, "New widget", false);
-		btnNewButton_3.setSize("100%", "100%");
+		SubmitButton = btnNewButton;
+		CancelButton = btnBack;
+		WorkIDTextBox = textBox;
+		SexTextBox = comboBox;
+		NameTextBox = textBox_1;
+		SurnameTextBox = textBox_2;
+		NameTTextBox = textBox_3;
+		SurnameTTextBox = textBox_4;
+		ShortNameTextBox = textBox_5;
 		
-		StackLayoutPanel stackLayoutPanel = new StackLayoutPanel(Unit.EM);
+		RoleTextBox = comboBox_1;
+		SegmentTextBox = comboBox_2;
 		
-		Button btnNewButton_7 = new Button("New button");
-		stackLayoutPanel.add(btnNewButton_7, new HTML("New Widget"), 2.0);
+		EmailTextBox = textBox_7;
+		PhoneTextBox = textBox_6;
 		
-		Button btnNewButton_8 = new Button("New button");
-		stackLayoutPanel.add(btnNewButton_8, new HTML("New Widget"), 2.0);
-		tabPanel.add(stackLayoutPanel, "New tab", false);
-		stackLayoutPanel.setSize("5cm", "3cm");
+		//SegmentTextBox.getS
 	}
+	
+	@Override
+	public Widget asWidget() {
+		// TODO Auto-generated method stub
+		
+		//System.out.println("return Widget : " +this	);
+		return this;
+	}
+
+	@Override
+	public HasClickHandlers getSubmit() {
+		// TODO Auto-generated method stub
+		return SubmitButton;
+	}
+
+	@Override
+	public HasClickHandlers getCancel() {
+		// TODO Auto-generated method stub
+		return CancelButton;
+	}
+
+	@Override
+	public HasValue<String> getWorkID() {
+		// TODO Auto-generated method stub
+		return WorkIDTextBox;
+	}
+
+	@Override
+	public HasValue<String> getName() {
+		// TODO Auto-generated method stub
+		return NameTextBox;
+	}
+
+	@Override
+	public HasValue<String> getSurname() {
+		// TODO Auto-generated method stub
+		return SurnameTextBox;
+	}
+
+	@Override
+	public HasValue<String> getNameT() {
+		// TODO Auto-generated method stub
+		return NameTTextBox;
+	}
+
+	@Override
+	public HasValue<String> getSurnameT() {
+		// TODO Auto-generated method stub
+		return SurnameTTextBox;
+	}
+
+	@Override
+	public HasValue<String> getShortname() {
+		// TODO Auto-generated method stub
+		return ShortNameTextBox;
+	}
+
+	@Override
+	public HasValue<String> getEmail() {
+		// TODO Auto-generated method stub
+		return EmailTextBox;
+	}
+
+	@Override
+	public HasValue<String> getPhone() {
+		// TODO Auto-generated method stub
+		return PhoneTextBox;
+	}
+
+	@Override
+	public String getSex() {
+		// TODO Auto-generated method stub
+		return SexTextBox.getItemText(SexTextBox.getSelectedIndex());
+	}
+
+	@Override
+	public String getRole() {
+		// TODO Auto-generated method stub
+		return RoleTextBox.getItemText(RoleTextBox.getSelectedIndex());
+	}
+	
+	
+	
+	@Override
+	public String getSegment() {
+		// TODO Auto-generated method stub
+		return SegmentTextBox.getItemText(SegmentTextBox.getSelectedIndex());
+	}
+
+	@Override
+	public ListBox setSex() {
+		// TODO Auto-generated method stub
+		return SexTextBox;
+	}
+
+	@Override
+	public ListBox setRole() {
+		// TODO Auto-generated method stub
+		return RoleTextBox;
+	}
+
+	@Override
+	public ListBox setSegment() {
+		// TODO Auto-generated method stub
+		return SegmentTextBox;
+	}
+
 
 }
